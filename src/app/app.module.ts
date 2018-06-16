15179a1db9fbc12f6d3b074dd85e39d8d6d6a9da
Jose Manuel Delgado Trueba <delgadotrueba@outlook.es>
2018-06-16 17:25:07 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//Importar Componentes
 import { AppComponent } from './app.component';
 import { MyComponente1Component } from './my-componente1/my-componente1.component';
 import { MyComponente2Component } from './my-componente2/my-componente2.component';
 import { MyComponente3Component } from './my-componente3/my-componente3.component';
 
+//Importar Modulos
+import { routing, appRoutingProviders } from './app.routing';
+
 @NgModule({
+  //Componentes
   declarations: [
     AppComponent,
     MyComponente1Component,
     MyComponente2Component,
     MyComponente3Component
   ],
+  //Modulos
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
   ],
-  providers: [],
+  //Servicos
+  providers: [ appRoutingProviders ],
+  //Arranque
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/app.routing.ts
@@ -0,0 +1,21 @@
+//Importar Modulos del router de Angular
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//Importar Componentes
+import { MyComponente1Component } from './my-componente1/my-componente1.component';
+import { MyComponente2Component } from './my-componente2/my-componente2.component';
+import { MyComponente3Component } from './my-componente3/my-componente3.component';
+
+// Array de rutas
+const  appRoutes: Routes = [
+    { path: '', component: MyComponente1Component },
+    { path: 'myComponente1', component: MyComponente1Component },
+    { path: 'myComponente2', component: MyComponente2Component },
+    { path: 'myComponente3', component: MyComponente3Component },
+    { path: '**', component: MyComponente1Component }, //Ruta 404
+];
+
+//Exportar el modulo del router
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
